5c1977f56ba5e7d8f288e4991d0b066d91083eea
Max Rohde <maxrohde.public@gmail.com>
2021-10-01 07:50:26 +1000

    format

--- packages/cli-app/scripts/build.js
+++ packages/cli-app/scripts/build.js
@@ -1,13 +1,12 @@
 const {pnpPlugin} = require('@yarnpkg/esbuild-plugin-pnp');
 const esbuild = require('esbuild');
 
-console.log('BUILD');
 esbuild.build({
   plugins: [pnpPlugin()],
   bundle: true,
   entryPoints: ['dist/src/run.js'],
   outfile: 'dist/cli.js',
 }).catch((e) => {
-  console.log('Build not successfult', e.message);
+  console.log('Build not successful', e.message);
   process.exit(1);}
 );
